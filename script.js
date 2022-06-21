7f565099a594928223a90ada647a27efa4b5c03f
ashishkj1105 <107899831+ashishkj1105@users.noreply.github.com>
2022-06-21 08:17:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,16 +27,16 @@ codes = {
   "y": "yankee",
   "z": "zulu",
   "@": "@",
-  ".": "dot"
-"0": "zero"
-"1": "one"
-"2":"two"
-"3":"three"
-"4":"four"
-"5":"five"
-"6":"six"
-"7":"seven"
-"8":"eight"
+  ".": "dot",
+"0": "zero",
+"1": "one",
+"2":"two",
+"3":"three",
+"4":"four",
+"5":"five",
+"6":"six",
+"7":"seven",
+"8":"eight",
 "9":"nine"
 };
 alphabet = prompt("Enter the word: ");
