d7463210bceeec110465234e28912dedf5f6ca2d
ashishkj1105 <107899831+ashishkj1105@users.noreply.github.com>
2022-06-21 07:55:27 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+var alphabet, codes, letter;
+codes = {
+  "a": "alpha",
+  "b": "bravo",
+  "c": "charlie",
+  "d": "delta",
+  "e": "echo",
+  "f": "foxtrot",
+  "g": "golf",
+  "h": "hotel",
+  "i": "India",
+  "j": "juliet",
+  "k": "kilo",
+  "l": "lima",
+  "m": "mike",
+  "n": "november",
+  "o": "oscar",
+  "p": "papa",
+  "q": "quebec",
+  "r": "romeo",
+  "s": "sierra",
+  "t": "tango",
+  "u": "uniform",
+  "v": "victor",
+  "w": "whiskey",
+  "x": "x-ray",
+  "y": "yankee",
+  "z": "zulu",
+  "@": "@",
+  ".": "dot"
+};
+alphabet = prompt("Enter the word: ");
+
+for (var x = 0, _pj_a = alphabet.length; x < _pj_a; x += 1) {
+  if (alphabet[x] === " ") {
+    document.write(" ");
+    continue;
+  }
+
+  letter = alphabet[x].toLowerCase();
+  document.write("<p><h1>",codes[letter].toUpperCase(),"</h1></p>");
+}
