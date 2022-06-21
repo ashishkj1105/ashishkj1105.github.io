7968868e3199f955794e24f803324079b988ed65
ashishkj1105 <107899831+ashishkj1105@users.noreply.github.com>
2022-06-21 08:12:23 +0530

    Update script.js
    Few more dicts

--- script.js
+++ script.js
@@ -28,6 +28,16 @@ codes = {
   "z": "zulu",
   "@": "@",
   ".": "dot"
+"0": "zero"
+"1": "one"
+"2":"two"
+"3":"three"
+"4":"four"
+"5":"five"
+"6":"six"
+"7":"seven"
+"8":"eight"
+"9":"nine"
 };
 alphabet = prompt("Enter the word: ");
 
