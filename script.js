822508e5f1b52bfd6931387abd29b864a01c1d1a
ashishkj1105 <107899831+ashishkj1105@users.noreply.github.com>
2022-06-24 13:36:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ codes = {
 "8":"eight",
 "9":"nine"
 };
-alphabet = String(prompt("Enter the word: "));
+var alphabet = String(prompt("Enter the word: "));
 document.getElementById('text').innerText=alphabet; 
 for (var x = 0, _pj_a = alphabet.length; x < _pj_a; x += 1) {
   if (alphabet[x] === " ") {
