8bac7236a5c200b3103bb6200b21c359faa12d9e
ashishkj1105 <107899831+ashishkj1105@users.noreply.github.com>
2022-06-24 13:55:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -55,13 +55,14 @@ function myFunction() {
   /* Get the text field */
   var copyText = alphabet;
 
-  /* Select the text field */
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+//   /* Select the text field */
+//   copyText.select();
+//   copyText.setSelectionRange(0, 99999); /* For mobile devices */
 
-   /* Copy the text inside the text field */
-  navigator.clipboard.writeText(copyText.value);
+//    /* Copy the text inside the text field */
+//   navigator.clipboard.writeText(copyText.value);
 
   /* Alert the copied text */
-  alert("Copied the text: " + copyText.value);
+//   alert("Copied the text: " + copyText.value);
+  alert(copyText);
 }
