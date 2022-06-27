22a4d993c14ce4a540cf1e71bac17caf3cf77dc4
ashishkj1105 <107899831+ashishkj1105@users.noreply.github.com>
2022-06-27 19:07:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -88,3 +88,13 @@ function reset() {
     document.getElementById("box").value = "";
     document.getElementById("box").focus();
 }
+
+document.getElementById("box").addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("sub").click();
+  }
+});
